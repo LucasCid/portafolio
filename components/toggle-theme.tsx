42b7a0285c3310a90bd95ad01b222fae4ca7ba1a
LucasCid <lucascidcanete@gmail.com>
2025-04-07 15:17:22 -0400

    error deploy

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import * as React from "react"
-import {MoonIcon, SunIcon} from "@radix-ui/react-icons"
+import React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
